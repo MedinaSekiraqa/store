df7a7ee7a280136570e2f3340cdbdcddbf7392d5
MedinaSekiraqa <102260547+MedinaSekiraqa@users.noreply.github.com>
2024-09-05 00:08:26 +0200

    some-changes

--- admindashboard/src/components/ui/form.tsx
+++ admindashboard/src/components/ui/form.tsx
@@ -3,9 +3,6 @@ import * as LabelPrimitive from "@radix-ui/react-label"
 import { Slot } from "@radix-ui/react-slot"
 import { Controller, ControllerProps, FieldPath, FieldValues, FormProvider, useFormContext } from "react-hook-form"
 
-import { cn } from "@/lib/utils"
-import { Label } from "@/components/ui/label"
-
 const Form = FormProvider
 
 type FormFieldContextValue<
@@ -14,15 +11,6 @@ type FormFieldContextValue<
 > = {
    name: TName
 }
-;<input
-   type={type}
-   className={cn(
-      "flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
-      className
-   )}
-   ref={ref}
-   {...props}
-/>
 
 const FormFieldContext = React.createContext<FormFieldContextValue>({} as FormFieldContextValue)
 
