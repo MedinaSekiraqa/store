27d3567c9aabf742e0ee912d3080f3a8df28cd29
MedinaSekiraqa <102260547+MedinaSekiraqa@users.noreply.github.com>
2024-09-07 00:45:13 +0200

    data-seed

--- admindashboard/src/hooks/use-store-modal.tsx
+++
@@ -1,7 +0,0 @@
-import { create } from "zustand"
-
-export const useStoreModal = create<useStoreModalStore>((set) => ({
-   isOpen: false,
-   onOpen: () => set({ isOpen: true }),
-   onClose: () => set({ isOpen: false }),
-}))
--- admindashboard/src/hooks/use-user.tsx
+++
@@ -1,6 +0,0 @@
-import { create } from "zustand"
-
-export const useUser = create<useUser>((set) => ({
-   role: null,
-   setRole: (role) => set({ role }),
-}))
